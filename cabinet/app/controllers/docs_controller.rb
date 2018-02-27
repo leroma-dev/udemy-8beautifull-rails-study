7d3a8b9b471ae6af4d73e8990e6821ab3ec50dd5
leroma-dev <p3computadores+dev@gmail.com>
2018-02-27 01:23:00 -0300

    Doc controller and routes

--- cabinet/app/controllers/docs_controller.rb
+++ cabinet/app/controllers/docs_controller.rb
@@ -6,26 +6,27 @@ class DocsController < ApplicationController
 	def show
 	end
 
-	def new
-	end
+	def new 
+	end 
 
 	def create
-	end
+	end 
 
 	def edit
-	end
+	end 
 
 	def update
-	end
+	end 
 
-	def destroy
-	end
+	def destroy 
+	end 
 
-	private
-	
-		def fin_doc
-		end
+	private 
+		def find_doc
+		end 
+
 
 		def doc_params
-		end
+		end 
+
 end
