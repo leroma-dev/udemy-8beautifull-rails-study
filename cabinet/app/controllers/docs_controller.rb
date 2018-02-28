8f34dc5639105626c3545b95fb058e44b8cb23c8
leroma-dev <p3computadores+dev@gmail.com>
2018-02-28 02:21:55 -0300

    edit and destroy docs

--- cabinet/app/controllers/docs_controller.rb
+++ cabinet/app/controllers/docs_controller.rb
@@ -29,14 +29,20 @@ class DocsController < ApplicationController
 	end 
 
 	def update
+		if @doc.update(doc_params)
+			redirect_to @doc
+		else
+			render "edit"
+		end
 	end 
 
 	def destroy 
+		@doc.destroy
+		redirect_to docs_path
 	end 
 
 	private 
 		def find_doc
-
 			@doc = Doc.find(params[:id])
 		end 
 
